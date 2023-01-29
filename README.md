eb46e3262c719f7cfa82ab227069b711216f78ac
Dave Cuza <cuz4@icloud.com>
2023-01-28 23:07:33 -0500

    Adding exit codes for failures and using env vars for sensitive data

--- README.md
+++ README.md
@@ -1,13 +1,16 @@
 # login_nauta_etecsa
+
 Este bash script permite correrlo mediante un crobjob dentro de su ordenador Linux o dentro de un router con OpenWRT y garantiza que la conexión a Internet con etecsa siempre se matenga activa.
 Es reomendado ejecutarlo cada 1 minuto.
 Olvídate de volver a iniciar sesión y disfruta la tarifa plana de NAUTAPLUS!!!
 
-Es importante darle permiso de ejecución al script una vez ubicado dentro de su equipo: CHMOD 755 login.sh
+Es importante darle permiso de ejecución al script una vez ubicado dentro de su equipo: `chmod +x login.sh`
 
-Enel ejemplo el archivo fue colocado en la carpeta /etc/crontabs/ .El cronjob pudiese ser configurado para ejecutarse cada 1 minuto: 
+En el ejemplo el archivo fue colocado en la carpeta /etc/crontabs/ .El cronjob pudiese ser configurado para ejecutarse cada 1 minuto: 
 
-* * * * * /etc/crontabs/login.sh
+```shell
+* * * * * NOMBRE_USUARIO="usuario@nautaplus" PASSWORD="su_password" /etc/crontabs/login.sh
+```
 
 Si el script detecta que hay enlace con etecsa (responde secure.etecsa.net) pero luego de postear el usuario/contraseña, no detecta internet (no responde 8.8.8.8) entonces, detiene el servicio de cronjobs. 
 
--- login.sh
+++ login.sh
@@ -1,56 +1,54 @@
 #!/bin/bash
- 
-#####################################################################
-# DATOS DE ACCESO A UTILIZAR (Conserva las comillas!!!)
-#####################################################################
-NOMBRE_USUARIO="usuario@nautaplus"
-PASSWORD="su_password"
-#####################################################################
- 
- 
- 
- 
+
+for var in NAUTA_USER NAUTA_PASS; do
+  if [ -n "${!var:-}" ]; then
+    echo "$var tiene asignado ${!var}"
+  else
+    echo "Error! La variable $var no está definida."
+    exit 1
+  fi
+done
+
 ######################################################################
 # Script Configuration
 ######################################################################
 TARGET_URL="https://secure.etecsa.net:8443/LoginServlet"
- 
+
 # Set the internet server to be verified (Change this as needed)
 VERIFICATION_SERVER="8.8.8.8"
- 
+
 # Verification Timeout (seconds)
 VERIFICATION_TIMEOUT_IN_SECONDS=1
- 
+
 # User Data
-#data="username=myuser&&password=mypass"
-data="username="${NOMBRE_USUARIO}"&&password="${PASSWORD}
- 
+data="username="${NAUTA_USER}"&&password="${NAUTA_PASS}
+
 # Checks if internet is reachable
-ping -c 1 -W ${VERIFICATION_TIMEOUT_IN_SECONDS} ${VERIFICATION_SERVER} &> /dev/null
- 
+ping -c 1 -W ${VERIFICATION_TIMEOUT_IN_SECONDS} ${VERIFICATION_SERVER} &>/dev/null
+
 if [[ $? -ne 0 ]]; then
-        # Internet not detected,
-        # post username and password to URL
-        echo "Internet NO responde. Intentando conectar..."
- 
-                if ping -c 1 -W 10 secure.etecsa.net &> /dev/null
-                then
-                curl --insecure -X POST ${TARGET_URL} -d "${data}"
-                echo "El enlace con ETECSA responde correctamente. Verificando que ya se haya conectado Internet..."
- 
- 
-                        if ping -c 1 -W 10 ${VERIFICATION_SERVER} &> /dev/null
-                        then
-                        echo "Internet se ha conectado CORRECTAMENTE"
-                        else
-                        /etc/init.d/cron stop
-                        echo "No fue posible conectarse. Deteniendo el servicio CRON"
-                        fi
-                else
-                echo "No hay conexion con ETECSA en estos momentos. No ha sido posible iniciar sesion. Continuaremos intentando conectar..."
-                fi
- 
+  # Internet not detected,
+  # post username and password to URL
+  echo "Internet NO responde. Intentando conectar..."
+
+  if ping -c 1 -W 10 secure.etecsa.net &>/dev/null; then
+    curl -k ${TARGET_URL} -d "${data}"
+    echo "El enlace con ETECSA responde correctamente. Verificando que ya se haya conectado Internet..."
+
+    if ping -c 1 -W 10 ${VERIFICATION_SERVER} &>/dev/null; then
+      echo "Internet se ha conectado CORRECTAMENTE"
+    else
+      /etc/init.d/cron stop
+      echo "No fue posible conectarse. Deteniendo el servicio CRON"
+      exit 1
+    fi
+
+  else
+    echo "No hay conexion con ETECSA en estos momentos. No ha sido posible iniciar sesion. Continuaremos intentando conectar..."
+    exit 1
+  fi
+
 else
-        # Internet active
-        echo "Internet responde correctamente al PING. Usted esta conectado"
+  # Internet active
+  echo "Internet responde correctamente al PING. Usted esta conectado"
 fi
